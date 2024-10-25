f36bc33cf849b87c9cfcf2bc37769037556304ff
Napoleao Barbosa da Cunha Junior <Napoleao51241156@edu.df.senac.br>
2024-10-25 11:25:21 -0300

    atualizar

--- herois/src/main/java/br/com/senac/herois/entity/Equipe.java
+++ herois/src/main/java/br/com/senac/herois/entity/Equipe.java
@@ -1,40 +1,54 @@
 package br.com.senac.herois.entity;
 
+import org.hibernate.mapping.Set;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "equipe")
 public class Equipe {
-     @Id
+
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-     @Column
+    @Column
     private String nome;
     @Column
     private String historiaOrigem;
-    
+
+    @OneToMany
+    @JoinColumn(name = "superHeroi_id", referencedColumnName = "id")
+    private SuperHeroi superHeroi;
+
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public String getNome() {
         return nome;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
     public String getHistoriaOrigem() {
         return historiaOrigem;
     }
+
     public void setHistoriaOrigem(String historiaOrigem) {
         this.historiaOrigem = historiaOrigem;
     }
-
 }
--- herois/src/main/java/br/com/senac/herois/entity/SuperHeroi.java
+++ herois/src/main/java/br/com/senac/herois/entity/SuperHeroi.java
@@ -1,14 +1,13 @@
 package br.com.senac.herois.entity;
 
 import java.util.Date;
-
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -18,89 +17,94 @@ public class SuperHeroi {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    @Column
-    private String nome;
+
     @Column
     private Date primeiraApricao;
+
     @Column
     private String apelido;
-    @Column
-    private String superPoder;
+
     @Column
     private String fraqueza;
+
     @Column
     private String historiaOrigem;
 
-    @OneToOne
-    @JoinColumn(name = "superHeroi_id", referencedColumnName = "id")
+    @Column
+    private String nome;
+
+    @Column
+    private String superPoder;
+
+        @ManyToOne
+    @JoinColumn(name = "equipe_id", referencedColumnName = "id")
     private Equipe equipe;
 
-    public Equipe getEquipe() {
-        return equipe;
-    }
+        public int getId() {
+            return id;
+        }
 
-    public void setEquipe(Equipe equipe) {
-        this.equipe = equipe;
-    }
+        public void setId(int id) {
+            this.id = id;
+        }
 
-    public String exibirDados() {
-        return getId() + getNome() + getPrimeiraApricao() + getApelido() + getSuperPoder() + getFraqueza() + getHistoriaOrigem();
-    }
+        public Date getPrimeiraApricao() {
+            return primeiraApricao;
+        }
+
+        public void setPrimeiraApricao(Date primeiraApricao) {
+            this.primeiraApricao = primeiraApricao;
+        }
+
+        public String getApelido() {
+            return apelido;
+        }
+
+        public void setApelido(String apelido) {
+            this.apelido = apelido;
+        }
+
+        public String getFraqueza() {
+            return fraqueza;
+        }
+
+        public void setFraqueza(String fraqueza) {
+            this.fraqueza = fraqueza;
+        }
+
+        public String getHistoriaOrigem() {
+            return historiaOrigem;
+        }
+
+        public void setHistoriaOrigem(String historiaOrigem) {
+            this.historiaOrigem = historiaOrigem;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public String getSuperPoder() {
+            return superPoder;
+        }
+
+        public void setSuperPoder(String superPoder) {
+            this.superPoder = superPoder;
+        }
+
+        public Equipe getEquipe() {
+            return equipe;
+        }
+
+        public void setEquipe(Equipe equipe) {
+            this.equipe = equipe;
+        }
+
+
+
     
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public Date getPrimeiraApricao() {
-        return primeiraApricao;
-    }
-
-    public void setPrimeiraApricao(Date primeiraApricao) {
-        this.primeiraApricao = primeiraApricao;
-    }
-
-    public String getApelido() {
-        return apelido;
-    }
-
-    public void setApelido(String apelido) {
-        this.apelido = apelido;
-    }
-
-    public String getSuperPoder() {
-        return superPoder;
-    }
-
-    public void setSuperPoder(String superPoder) {
-        this.superPoder = superPoder;
-    }
-
-    public String getFraqueza() {
-        return fraqueza;
-    }
-
-    public void setFraqueza(String fraqueza) {
-        this.fraqueza = fraqueza;
-    }
-
-    public String getHistoriaOrigem() {
-        return historiaOrigem;
-    }
-
-    public void setHistoriaOrigem(String historiaOrigem) {
-        this.historiaOrigem = historiaOrigem;
-    }
-
 }

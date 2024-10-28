36db6eecad8662c402e0b641a4f66cb3f9016609
Napoleao Barbosa da Cunha Junior <Napoleao51241156@edu.df.senac.br>
2024-10-28 11:11:12 -0300

    atualizar

--- herois/src/main/java/br/com/senac/herois/entity/Equipe.java
+++ herois/src/main/java/br/com/senac/herois/entity/Equipe.java
@@ -1,54 +1,58 @@
 package br.com.senac.herois.entity;
-
-import org.hibernate.mapping.Set;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-
 @Entity
 @Table(name = "equipe")
 public class Equipe {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @Column
     private String nome;
     @Column
+    private String apelido;
+    @Column
     private String historiaOrigem;
-
-    @OneToMany
-    @JoinColumn(name = "superHeroi_id", referencedColumnName = "id")
-    private SuperHeroi superHeroi;
-
+    @OneToMany(mappedBy = "equipe")
+    private Set<SuperHeroi> superHeroi;
+    public String getApelido() {
+        return apelido;
+    }
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
     public String getNome() {
         return nome;
     }
-
     public void setNome(String nome) {
         this.nome = nome;
     }
-
     public String getHistoriaOrigem() {
         return historiaOrigem;
     }
-
     public void setHistoriaOrigem(String historiaOrigem) {
         this.historiaOrigem = historiaOrigem;
+    }
+    public void setSuperHeroi(Set<SuperHeroi> superHeroi) {
+        this.superHeroi = superHeroi;
+    }
+    public Set<SuperHeroi> getsuperHeroi() {
+        return superHeroi;
+    }
+    public void setHerois(Set<SuperHeroi> superHeroi) {
+        this.superHeroi = superHeroi;
+    }
+    public void setApelido(String apelido) {
+        this.apelido = apelido;
     }
 }
--- herois/src/main/java/br/com/senac/herois/entity/SuperHeroi.java
+++ herois/src/main/java/br/com/senac/herois/entity/SuperHeroi.java
@@ -1,13 +1,13 @@
 package br.com.senac.herois.entity;
-
 import java.util.Date;
-import jakarta.persistence.ManyToOne;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -36,7 +36,7 @@ public class SuperHeroi {
     @Column
     private String superPoder;
 
-        @ManyToOne
+    @ManyToOne
     @JoinColumn(name = "equipe_id", referencedColumnName = "id")
     private Equipe equipe;
 
@@ -102,9 +102,5 @@ public class SuperHeroi {
 
         public void setEquipe(Equipe equipe) {
             this.equipe = equipe;
-        }
-
-
-
-    
+        }    
 }
